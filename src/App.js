e7f8a52e868b13f8281bf34744cf0a3a2daf7ce4
hali241997 <mhali241997@gmail.com>
2021-12-05 03:38:32 +0500

    update code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
-import {Button, I18nManager, SafeAreaView, Text} from 'react-native';
-import RNRestart from 'react-native-restart';
+import {Button, SafeAreaView, Text} from 'react-native';
 import './i18n';
 
 const App = () => {
@@ -9,15 +8,18 @@ const App = () => {
 
   return (
     <SafeAreaView>
-      <Text style={{fontSize: 22, textAlign: 'left'}}>{t('hello')}</Text>
+      <Text
+        style={{
+          fontSize: 22,
+          textAlign: i18n.language === 'ar' ? 'right' : 'left',
+        }}>
+        {t('hello')}
+      </Text>
 
       <Button
         title="Change Language"
         onPress={() => {
-          i18n.changeLanguage(i18n.language === 'ar' ? 'en' : 'ar').then(() => {
-            I18nManager.forceRTL(i18n.language === 'ar');
-            RNRestart.Restart();
-          });
+          i18n.changeLanguage(i18n.language === 'ar' ? 'en' : 'ar');
         }}
       />
     </SafeAreaView>
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,8 +1,35 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import {I18nManager} from 'react-native';
-import en from '../lang/en.json';
+import * as RNLocalize from 'react-native-localize';
 import ar from '../lang/ar.json';
+import en from '../lang/en.json';
+
+const languageDetector = {
+  type: 'languageDetector',
+  async: true,
+  detect: callback => {
+    AsyncStorage.getItem('APP_LANG', (err, lng) => {
+      if (err || !lng) {
+        if (err) {
+          console.log('Error fetching "APP_LANG" from async store', err);
+        } else {
+          console.log(
+            'No language is set, choosing the best available or English as fallback',
+          );
+        }
+        const bestLng = RNLocalize.findBestAvailableLanguage(['en', 'ar']);
+
+        callback(bestLng.languageTag ?? 'en');
+        return;
+      }
+      callback(lng);
+    });
+  },
+  cacheUserLanguage: lng => {
+    AsyncStorage.setItem('APP_LANG', lng);
+  },
+};
 
 const resources = {
   en: {
@@ -13,12 +40,17 @@ const resources = {
   },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: I18nManager.isRTL ? 'ar' : 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    react: {
+      useSuspense: false,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
